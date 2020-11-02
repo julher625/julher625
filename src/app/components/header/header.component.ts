39e4973a6e58d97ebc2d477fb24892527a507a1a
Julian Alejandro Hernandez Bohorquez <j.hernandez.bo@gmail.com>
2020-11-02 17:11:51 -0500

    Header v1.0

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core'; // OnInit se llama apenas inicia el componente 
+
+// Los que tienen @ son decoradores
+@Component({
+  selector: 'app-header', // Como lo llamare en el HTML principal
+  templateUrl: './header.component.html', // Ubicación del HTML
+  styleUrls: ['./header.component.sass'] // Ubicación del SASS (CSS con esteroides)
+})
+
+export class HeaderComponent {
+
+  constructor() { }
+
+  elements: Array<string> = ['Home', 'About', 'Proyects', 'Contact'];
+
+
+}
