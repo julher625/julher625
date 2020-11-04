1f1d75430f340976f3bae58d44ec2a88144f829f
Julian Alejandro Hernandez Bohorquez <j.hernandez.bo@gmail.com>
2020-11-03 23:04:33 -0500

    Contact

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+import {trigger, style, animate, transition, keyframes} from '@angular/animations';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.sass']
+  styleUrls: ['./contact.component.sass'],
+  animations: [
+    trigger('aniHome', [
+      transition('init => start', animate('1.2s ease-in', keyframes([
+        style({opacity: 0, offset: 0}),
+        style({opacity: 1, offset: 1})
+      ]))),
+    ])
+  ]
 })
-export class ContactComponent implements OnInit {
+export class ContactComponent implements AfterViewInit {
 
-  constructor() { }
+  public state = 'init';
 
-  ngOnInit(): void {
+  constructor() {}
+
+  ngAfterViewInit(): void {
+    setTimeout(() => {
+      this.init();
+    });
+  }
+
+  init(): void {
+    this.state = 'start';
+    console.log(this.state);
   }
 
 }
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, AfterViewInit } from '@angular/core';
-import {trigger, state, style, animate, transition, keyframes} from '@angular/animations';
+import {trigger, style, animate, transition, keyframes} from '@angular/animations';
 
 @Component({
   selector: 'app-home',
